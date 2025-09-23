0ac12bd75b0ae37f0300e13ffe8e16e29ce92874
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-09-23 12:27:31 -0400

    Says the temperature in Kelvin

--- main.ts
+++ main.ts
@@ -2,10 +2,16 @@
  *
  * Created by: Jack
  * Created on: Sep 2025
- * This program shows a smiley face
+ * This program says the temperature in Kelvin
 */
+
+// variable for the temperature of the microbit
+let temperatureOfMicrobit: number = 0
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-//comment
+input.onButtonPressed(Button.A, function () {
+    temperatureOfMicrobit = input.temperature() + 273
+    basic.showString(" The temperature is: " + temperatureOfMicrobit.toString() + " K")
+})
